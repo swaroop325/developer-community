9d693d6940f51e5e3650ccac85b4be35b70abd29
Anantan08 <59593294+Anantan08@users.noreply.github.com>
2020-01-30 22:17:28 +0530

    Create COFI.py
    created by ANANTAN L K

---
+++ COFI.py
@@ -0,0 +1,5 @@
+distance = int(input("enter travel distance (in km): "))
+mileage = int(input("enter milage of your vehicle(in litre): "))
+COF = int(input("enter the cost of fuel(per litre) : "))
+result = (distance/mileage)*COF
+print("result: ",result)
